291879ffe3ab5ece6a20204a5b4346e7a0b73675
imadege <imadege1990@gmail.com>
2017-01-03 11:51:54 +0300

    Fixed updates on sms

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='JHLSMS',
+setup(name='sms',
       version='0.1',
       description='Sms package Intergrated with twilio',
       url='https://github.com/imadege/jhlsms',
       author='Ian Madege',
       author_email='imadege1990@gmail.com',
       license='MIT',
-      packages=['SmsJhl'],
+      packages=['sms'],
       zip_safe=False)
--- sms/__init__.py
+++ sms/__init__.py
@@ -1,1 +1,6 @@
 from sms.Sms import SendMessage
+
+
+def send(gateway,number,message):
+    s = SendMessage(gateway,number,message)
+    return True
