c8b71718376e857f3f051e0ad1f318ec84e48cf2
Ian Madege <imadege1990@gmail.com>
2017-03-04 16:54:03 +0300

    updated code

--- sms/Sms.py
+++ sms/Sms.py
@@ -54,13 +54,6 @@ class SendMessage():
         try:
             # Thats it, hit send and we'll take care of the rest.
             results = gateway.sendMessage(to, message)
-
-            for recipient in results:
-                # status is either "Success" or "error message"
-                print 'number=%s;status=%s;messageId=%s;cost=%s' % (recipient['number'],
-                                                                    recipient['status'],
-                                                                    recipient['messageId'],
-                                                                    recipient['cost'])
         except AfricasTalkingGatewayException, e:
             print 'Encountered an error while sending: %s' % str(e)
 
